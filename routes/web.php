c28d76368636e3cb3e80a80a4b4084db794b24a1
stydfhrl <fahrilsetyoadi@gmail.com>
2022-07-28 22:26:31 +0700

    view mentah

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('catatan.viewcatatan');
 });
+
+
+route::get('/catatan', [CatatanController::class, 'index'])->name('catatan');
